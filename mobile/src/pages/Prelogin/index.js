e1a7d1557f148d8de7e2423158c845fff45422f5
Beluomini <lucasbeluomini1@gmail.com>
2023-11-20 16:06:37 -0300

    tela pre login, pergunta se possui ou não a conta

---
+++ mobile/src/pages/Prelogin/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, Image, Pressable } from 'react-native';
+import styles from './styles';
+
+import frozen_food from '../../assets/frozen_food.png';
+
+export default function WelcomePage({ navigation, route}) {
+
+    return (
+        <View style={styles.container}>
+            <View  style={styles.page}>
+                <View style={styles.image}>
+                    <Image source={frozen_food} />
+                </View>
+                <Text style={styles.title} >Cadastre-se para começar</Text>
+                <Pressable style={styles.button} onPress={() => navigation.navigate('Register')}>
+                    <Text style={styles.buttonText}>Criar uma conta</Text>
+                </Pressable>
+                <Pressable style={styles.link} onPress={() => navigation.navigate('Login')}>
+                    <Text style={styles.linkText}>Já tenho uma conta</Text>
+                </Pressable>
+                <StatusBar style="auto" />
+            </View>
+        </View>
+    );
+}
+
---
+++ mobile/src/pages/Prelogin/styles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    page: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image:{
+        width: "75%",
+        marginBottom: "15%",
+    },
+    title: {
+        fontFamily: 'Roboto',
+        fontSize: 25,
+        color: '#000000',
+        marginBottom: "15%",
+    },
+    button: {
+        width: "80%",
+        marginBottom: "10%",
+    },
+    buttonText: {
+        fontFamily: 'Roboto',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        backgroundColor: '#D33333',
+        padding: 10,
+        paddingHorizontal: 40,
+        textAlign: 'center',
+        borderRadius: 40,
+        letterSpacing: 1,
+    },
+    link: {
+        width: "80%",
+    },
+    linkText: {
+        fontFamily: 'Roboto',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#D33333',
+        textAlign: 'center',
+        letterSpacing: 1,
+    },
+  });
+
+export default styles;
