889444e0f50dd5985acdd10495253f30660efa48
Beluomini <lucasbeluomini1@gmail.com>
2023-11-23 13:07:17 -0300

    rotas para busca de usuário por email ou id

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -24,6 +24,16 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Get('email/:email')
+  async findOneByEmail(@Param('email') email: string) {
+    return this.userService.findOneByEmail(email);
+  }
+
   @Put(':id')
   async update(@Param('id') id: string, @Body() data: UserDTO) {
     return this.userService.update(id, data);
