7acad7f1f03ebaeca30cc85f8368b5e5d1792852
Beluomini <lucasbeluomini1@gmail.com>
2023-12-05 18:16:41 -0300

    criação de um guard e uma stratege para o login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/modules/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    validateUser(email: string, password: string) {
+      throw new Error('Method not implemented.');
+    }
+
+}
