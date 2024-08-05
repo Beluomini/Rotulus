6ce5c08f649abf92bdb4c96d2538f21352ea5b7f
Beluomini <lucasbeluomini1@gmail.com>
2024-08-05 15:39:07 -0300

    criando tratativa de erros para as rotas de classificação

--- backend/src/modules/classification/classification.controller.ts
+++ backend/src/modules/classification/classification.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClassificationService } from './classification.service';
 import { ClassificationDTO } from './classification.dto';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Classification')
 @Controller('classification')
@@ -10,6 +18,8 @@ export class ClassificationController {
   constructor(private readonly classificationService: ClassificationService) {}
 
   @Post()
+  @ApiResponse({ status: 409, description: 'Classificação já cadastrada' })
+  @ApiResponse({ status: 500, description: 'Erro ao criar classificação' })
   async create(@Body() data: ClassificationDTO) {
     return this.classificationService.create(data);
   }
@@ -22,6 +32,7 @@ export class ClassificationController {
 
   @IsPublic()
   @Get(':id')
+  @ApiResponse({ status: 404, description: 'Classificação não encontrada' })
   async findOne(@Param('id') id: string) {
     return this.classificationService.findOne(id);
   }
@@ -33,11 +44,21 @@ export class ClassificationController {
   }
 
   @Put(':id')
+  @ApiResponse({ status: 404, description: 'Classificação não encontrada' })
+  @ApiResponse({ status: 409, description: 'Nome para classificação já usado' })
+  @ApiResponse({ status: 500, description: 'Erro ao atualizar classificação' })
   async update(@Param('id') id: string, @Body() data: ClassificationDTO) {
     return this.classificationService.update(id, data);
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 404, description: 'Classificação não encontrada' })
+  @ApiResponse({
+    status: 409,
+    description:
+      'Classificação não pode ser deletada pois possui vinculo com alimento',
+  })
+  @ApiResponse({ status: 500, description: 'Erro ao deletar classificação' })
   async delete(@Param('id') id: string) {
     return this.classificationService.delete(id);
   }
--- backend/src/modules/classification/classification.service.ts
+++ backend/src/modules/classification/classification.service.ts
@@ -1,111 +1,153 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { ClassificationDTO } from './classification.dto';
 
 @Injectable()
 export class ClassificationService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService) {}
+  async create(data: ClassificationDTO) {
+    const classificationExists = await this.prisma.classification.findFirst({
+      where: {
+        name:
+          data.name.charAt(0).toUpperCase() + data.name.slice(1).toLowerCase(),
+      },
+    });
 
-    async create(data: ClassificationDTO) {
-
-        const classificationExists = await this.prisma.classification.findFirst({
-            where: {
-                name: data.name.toLowerCase(),
-            }
-        });
-
-        if (classificationExists) {
-            throw new Error('Esta classificação já existe');
-        }
-
-        // transforma o nome da classificação caixa baixa com a primeira letra da primeira palavra maiúscula
-        data.name = data.name.charAt(0).toUpperCase()+data.name.slice(1).toLowerCase();
-
-        const classification = await this.prisma.classification.create({
-            data: {
-                name: data.name,
-                description: data.description,
-            }
-        });
-        return classification;
+    if (classificationExists) {
+      throw new ConflictException('Classificação já cadastrada');
     }
 
-    async findAll() {
-        return await this.prisma.classification.findMany();
+    // transforma o nome da classificação caixa baixa com a primeira letra da primeira palavra maiúscula
+    data.name =
+      data.name.charAt(0).toUpperCase() + data.name.slice(1).toLowerCase();
+
+    try {
+      const classification = await this.prisma.classification.create({
+        data: {
+          name: data.name,
+          description: data.description,
+        },
+      });
+      return classification;
+    } catch (err) {
+      throw new InternalServerErrorException('Erro ao criar classificação');
+    }
+  }
+
+  async findAll() {
+    return await this.prisma.classification.findMany();
+  }
+
+  async findOne(id: string) {
+    const classification = await this.prisma.classification.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!classification) {
+      throw new NotFoundException('Classificação não encontrada');
     }
 
-    async findOne(id: string) {
-        const classification = await this.prisma.classification.findUnique({
-            where: {
-                id: id,
-            }
-        });
-        return classification;
+    return classification;
+  }
+
+  async findManyByName(name: string) {
+    const classifications = await this.prisma.classification.findMany({
+      where: {
+        name: {
+          contains: name,
+        },
+      },
+    });
+    return classifications;
+  }
+
+  async update(id: string, data: ClassificationDTO) {
+    const classificationExists = await this.prisma.classification.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!classificationExists) {
+      throw new NotFoundException('Classificação não encontrada');
     }
 
-    async findManyByName(name: string) {
-        const classifications = await this.prisma.classification.findMany({
-            where: {
-                name: {
-                    contains: name,
-                }
-            }
-        });
-        return classifications;
+    if (data.name) {
+      const classNameUsed = await this.prisma.classification.findFirst({
+        where: {
+          name:
+            data.name.charAt(0).toUpperCase() +
+            data.name.slice(1).toLowerCase(),
+        },
+      });
+
+      if (classNameUsed) {
+        throw new ConflictException(
+          'Este nome de classificação já está sendo usado',
+        );
+      }
     }
 
-    async update(id: string, data: ClassificationDTO) {
-            
-            const classificationExists = await this.prisma.classification.findUnique({
-                where: {
-                    id: id,
-                }
-            });
-    
-            if (!classificationExists) {
-                throw new Error('Classificação não encontrada');
-            }
-    
-            const classification = await this.prisma.classification.update({
-                where: {
-                    id: id,
-                },
-                data: {
-                    name: data.name === undefined ? classificationExists.name : data.name.toLowerCase(),
-                    description: data.description,
-                }
-            });
-            return classification;
-        }
+    try {
+      const classification = await this.prisma.classification.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name:
+            data.name === undefined
+              ? classificationExists.name
+              : data.name.charAt(0).toUpperCase() +
+                data.name.slice(1).toLowerCase(),
+          description: data.description,
+        },
+      });
+      return classification;
+    } catch (err) {
+      throw new InternalServerErrorException('Erro ao editar classificação');
+    }
+  }
 
-    async delete(id: string) {
-        const classificationExists = await this.prisma.classification.findUnique({
-            where: {
-                id: id,
-            }
-        });
-        
-        if (!classificationExists) {
-            throw new Error('Classificação não encontrada');
-        }
+  async delete(id: string) {
+    const classificationExists = await this.prisma.classification.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-        const foodExists = await this.prisma.food.findFirst({
-            where: {
-                classificationId: id,
-            }
-        });
-
-        if (foodExists) {
-            throw new Error('Esta classificação não pode ser excluída, pois está sendo usada por um alimento');
-        }
-
-        const classification = await this.prisma.classification.delete({
-            where: {
-                id: id,
-            }
-        });
-        return classification;
+    if (!classificationExists) {
+      throw new NotFoundException('Classificação não encontrada');
     }
 
+    const foodExists = await this.prisma.food.findFirst({
+      where: {
+        classificationId: id,
+      },
+    });
+
+    if (foodExists) {
+      throw new ConflictException(
+        'Classificação não pode ser deletada pois está relacionada à um alimento',
+      );
+    }
+
+    try {
+      const classification = await this.prisma.classification.delete({
+        where: {
+          id: id,
+        },
+      });
+      return classification;
+    } catch (err) {
+      throw new InternalServerErrorException('Erro ao deletar classificação');
+    }
+  }
 }
