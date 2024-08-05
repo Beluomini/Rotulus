3d60f40ae97dcfe9542cf7ee820a468ed4256721
Beluomini <lucasbeluomini1@gmail.com>
2024-08-05 15:39:38 -0300

    corrigindo o tipo do código de barras para string com decorators

--- backend/src/modules/food/food.dto.ts
+++ backend/src/modules/food/food.dto.ts
@@ -1,95 +1,96 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class FoodDTO {
+  @ApiProperty()
+  id?: string;
 
-    @ApiProperty()
-    id?: string;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    name: string;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    barcode: string;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    brandName: string;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    servingSize: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    energyValue: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    carbohydrate: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    totalSugar: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    addedSugar: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    protein: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    totalFat: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    saturatedFat: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    transFat: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    fiber: number;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    sodium: number;
-  
-    @ApiProperty()
-    listIngredients?: string;
-  
-    @ApiProperty()
-    description?: string;
-  
-    @ApiProperty()
-    image?: string;
-  
-    @ApiProperty()
-    @IsNotEmpty()
-    classificationId: string;
-  
-    @ApiProperty()
-    ingredients?: string[];
-  
-    @ApiProperty()
-    additives?: string[];
-  };
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  barcode: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  brandName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  servingSize: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  energyValue: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  carbohydrate: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  totalSugar: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  addedSugar: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  protein: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  totalFat: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  saturatedFat: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  transFat: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  fiber: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  sodium: number;
+
+  @ApiProperty()
+  listIngredients?: string;
+
+  @ApiProperty()
+  description?: string;
+
+  @ApiProperty()
+  image?: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  classificationId: string;
+
+  @ApiProperty()
+  ingredients?: string[];
+
+  @ApiProperty()
+  additives?: string[];
+}
